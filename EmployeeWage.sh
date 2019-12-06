37f0ce68ed3087e398ada33c123d1bad6851e833
kajal764 <kajaldw666@gmail.com>
2019-12-06 18:36:52 +0530

    Added : DailyWage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 
+WAGE_PER_HOUR=20
+FULL_DAY=8
+dailyWage=0
+
 attendance=$((RANDOM%2))
-if [[ attendance -eq 0 ]]
+
+if [[ $attendance -eq 0 ]]
 then
 	echo "employee is absent"
 else
 	echo "employee is present"
+	dailyWage=$(($FULL_DAY*$WAGE_PER_HOUR))
+	echo "Daily wage is $dailyWage"
 fi
-
