3fa4f65e687d414e0f7e221c291b44574041ca8b
kajal764 <kajaldw666@gmail.com>
2019-12-06 19:14:24 +0530

    Calculate: DailyWage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,16 +3,24 @@ echo "Welcome to Employee Wage Computation"
 
 WAGE_PER_HOUR=20
 FULL_DAY_WORKING_HOUR=8
-HAIF_DAY_WORKING_HOUR=4
+HALF_DAY_WORKING_HOUR=4
 dailyWage=0
 
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 
-if [[ $attendance -eq 0 ]]
-then
+case $attendance in
+	0)
 	echo "employee is absent"
-else
+	;;
+	1)
 	echo "employee is present"
-	dailyWage=$(($FULL_DAY*$WAGE_PER_HOUR))
+	dailyWage=$(( $HALF_DAY_WORKING_HOUR * $WAGE_PER_HOUR ))
 	echo "Daily wage is $dailyWage"
-fi
+	;;
+	2)
+	echo "employee is present"
+	dailyWage=$(( $FULL_DAY_WORKING_HOUR * $WAGE_PER_HOUR ))
+	echo "Daily wage is $dailyWage"
+	;;
+esac
+
