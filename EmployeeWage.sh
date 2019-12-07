5087d63bcae66ef0a60b5a73aa6e4138b8909c34
kajal764 <kajaldw666@gmail.com>
2019-12-07 15:39:08 +0530

    Added: daily wage and total Wage using array

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,9 +4,12 @@ echo "Welcome to Employee Wage Computation"
 WAGE_PER_HOUR=20
 FULL_DAY_WORKING_HOUR=8
 HALF_DAY_WORKING_HOUR=4
-dailyWage=0
-Total_working_hours=0
+
+declare -a daily_Wage
+declare -a Total_Wage
 days=0
+Total_Wage
+Total_Working_Hours=0
 function getHours(){
 	dailyhrs=$1
 	case $dailyhrs in
@@ -28,12 +31,19 @@ while [[ $Total_working_hours -le 100 && $days -le 20 ]]
 do
 	attendance=$((RANDOM%2))
 	hrs=$(getHours $attendance)
-	dailyWage=$(( $hrs * $WAGE_PER_HOUR ))
-   Total_working_hours=$(($Total_working_hours + $hrs))
+	daily=$(( $hrs * $WAGE_PER_HOUR ))
 
-days=$(($days+1))
+	daily_Wage[days]=$daily
+   Total_Working_Hours=$(($Total_Working_Hours + $hrs))
+	Total=$(( $Total+$daily ))
+	Total_Wage[days]=$Total
+	days=$(($days+1))
 done
 
-echo "total working hours $Total_working_hours"
+echo "daily wage array is "
+echo ${daily_Wage[@]}
+
+echo "total wage array is"
+echo ${Total_Wage[@]}
 
 
